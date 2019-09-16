5b59d1346ce4d7266ca5c49b045e050056b36756
Pavan Gopal <pavan.gopal@lenskart.in>
2019-09-17 00:45:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+
+app.set('view engine','hbs')
+app.use(express.static(__dirname+'/public'))
+
+
+app.use((req,res,next)=>{
+res.render('maintenance.hbs')
+
+})
+
+hbs.registerHelper('getCurrentYear',()=>{
+return new Date().getFullYear()
+})
+
+hbs.registerHelper("screemIt",(text)=>{
+return text.toUpperCase()
+})
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        name:'pavan',
+        title:'this is title'
+    })
+ })
+
+app.get('/about',(req,res)=>{
+   res.render('about.hbs',{
+       title:'this is title'
+   })
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running at 3000!");
+    
+});
